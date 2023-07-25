bb334d47fc72323bcb4d6ebdbd375b89ae0a407a
mr-kevinduy <mr.kevinduy@gmail.com>
2023-07-25 07:11:25 +0700

    h 20230724

---
+++ packages/kevinduy/backend/src/Builder/Layout/Column.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Kevinduy\Backend\Builder\Layout;
+
+use Illuminate\Contracts\Support\Renderable;
+
+class Column
+{
+    protected $width = [];
+    protected $contents = [];
+
+    public function __construct($content = null, $width = 12)
+    {
+        $this->width['md'] = $width;
+        $this->append($content);
+    }
+
+    public function append($content)
+    {
+        $this->contents[] = $content;
+
+        return $this;
+    }
+
+    public function build()
+    {
+        $output = '<div class="col">';
+        $output .= '<div class="col-wrapper">';
+
+        foreach ($this->contents as $content) {
+            if ($content instanceof Renderable) {
+                $output .= $content->render();
+            } else {
+                $output .= (string) $content;
+            }
+        }
+
+        $output .= '</div>';
+        $output .= '</div>';
+
+        echo $output;
+    }
+}
--- packages/kevinduy/backend/src/Builder/Layout/Content.php
+++ packages/kevinduy/backend/src/Builder/Layout/Content.php
@@ -6,8 +6,37 @@ use Illuminate\Contracts\Support\Renderable;
 
 class Content implements Renderable
 {
+    protected $rows = [];
+
     public function __construct()
     {
 
+    }
+
+    public function row($content)
+    {
+        if (is_callable($content)) {
+            $row = new Row();
+            call_user_func($content, $row);
+            $this->addRow($row);
+        } else {
+            $this->addRow(new Row($content));
+        }
+
+        return $this;
+    }
+
+    protected function addRow($row)
+    {
+        $this->rows[] = $row;
+    }
+
+    public function render()
+    {
+        $compact = [
+
+        ];
+
+        return view('layouts.backend', $compact);
     }
 }
